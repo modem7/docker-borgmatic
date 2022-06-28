a611b812e28c75fb3eba51a55c0d6fdf7b7730b0
modem7 <modem7@gmail.com>
2022-06-28 17:03:02 +0100

    add versions to entry.sh
    changed how versions are printed

--- base-dockercli/entry.sh
+++ base-dockercli/entry.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
+
 # Import your cron file
 /usr/bin/crontab /etc/borgmatic.d/crontab.txt
+
 #Variables
+dockerver=$(docker --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
 borgver=$(borg --version)
 borgmaticver=$(borgmatic --version)
+apprisever=$(apprise --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
+
 #Software versions
-docker --version
+echo docker $dockerver
 echo borgmatic $borgmaticver
 echo $borgver
+echo apprise $apprisever
+
 # Start cron
 /usr/sbin/crond -f -L /dev/stdout
--- base-liveinstall/entry.sh
+++ base-liveinstall/entry.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 # Variables
+dockerver=$(docker --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
 borgver=$(borg --version)
 borgmaticver=$(borgmatic --version)
+apprisever=$(apprise --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
 
 # Wait for internet connection
 	echo "Waiting for Internet connection...."
@@ -23,9 +25,10 @@ apk update -q && apk add --no-cache -q ${LIVEINSTALL:-docker-cli}
 /usr/bin/crontab /etc/borgmatic.d/crontab.txt
 
 # Software versions
-docker --version
+echo docker $dockerver
 echo borgmatic $borgmaticver
 echo $borgver
+echo apprise $apprisever
 
 # Start cron
 /usr/sbin/crond -f -L /dev/stdout
