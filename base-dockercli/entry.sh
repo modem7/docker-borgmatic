61eb0d557fbda6f703e38f243617bb8f064e6f27
modem7 <modem7@gmail.com>
2022-08-19 00:04:59 +0100

    allow for extra cron commands after initial

--- base-dockercli/entry.sh
+++ base-dockercli/entry.sh
@@ -1,17 +1,11 @@
-#!/bin/sh
-
-# Generate Cron variables
-export CRON=${CRON:-"0 1 * * *"}
-export CRON_COMMAND=${CRON_COMMAND:-"borgmatic --stats -v 0 2>&1"}
-
-# Output cron settings to console
-echo "Cron job set as: \"$CRON $CRON_COMMAND\""
+#!/bin/bash
 
 # Version variables
 dockerver=$(docker --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
 borgver=$(borg --version)
 borgmaticver=$(borgmatic --version)
 apprisever=$(apprise --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
+crontab=$(crontab -l)
 
 # Software versions
 echo docker $dockerver
@@ -19,6 +13,19 @@ echo borgmatic $borgmaticver
 echo $borgver
 echo apprise $apprisever
 
-# Start cron
+# Generate Cron variables
+CRON="${CRON:-"0 1 * * *"}"
+CRON_COMMAND="${CRON_COMMAND:-"borgmatic --stats -v 0 2>&1"}"
+
+# Apply cron variables
 echo "$CRON $CRON_COMMAND" > /etc/crontabs/root
+if [ -v EXTRA_CRON ]
+then
+   echo "$EXTRA_CRON" >> /etc/crontabs/root
+fi
+
+# Output cron settings to console
+printf "Cron job set as: \n$crontab\n"
+
+# Start Cron
 /usr/sbin/crond -f -L /dev/stdout
--- base-liveinstall/entry.sh
+++ base-liveinstall/entry.sh
@@ -1,19 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-# Generate Cron variables
-export CRON=${CRON:-"0 1 * * *"}
-export CRON_COMMAND=${CRON_COMMAND:-"borgmatic --stats -v 0 2>&1"}
-
-# Variables
+# Version variables
 dockerver=$(docker --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
 borgver=$(borg --version)
 borgmaticver=$(borgmatic --version)
 apprisever=$(apprise --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
+crontab=$(crontab -l)
 
 # Wait for internet connection
-	echo "Waiting for Internet connection...."
-		./wtfc.sh -Q -T 5 ping -c 4 www.google.com
-	echo "Internet connection established"
+    echo "Waiting for Internet connection...."
+        ./wtfc.sh -Q -T 5 ping -c 4 www.google.com
+    echo "Internet connection established"
 
 # Check variable to see if present, echo result
 if [ -z "$LIVEINSTALL" ]
@@ -26,15 +23,25 @@ fi
 # Installing packages
 apk update -q && apk add --no-cache -q ${LIVEINSTALL:-docker-cli}
 
-# Output cron settings to console
-echo "Cron job set as: \"$CRON $CRON_COMMAND\""
-
 # Software versions
 echo docker $dockerver
 echo borgmatic $borgmaticver
 echo $borgver
 echo apprise $apprisever
 
-# Start cron
+# Generate Cron variables
+CRON="${CRON:-"0 1 * * *"}"
+CRON_COMMAND="${CRON_COMMAND:-"borgmatic --stats -v 0 2>&1"}"
+
+# Apply cron variables
 echo "$CRON $CRON_COMMAND" > /etc/crontabs/root
+if [ -v EXTRA_CRON ]
+then
+   echo "$EXTRA_CRON" >> /etc/crontabs/root
+fi
+
+# Output cron settings to console
+printf "Cron job set as: \n$crontab\n"
+
+# Start Cron
 /usr/sbin/crond -f -L /dev/stdout
