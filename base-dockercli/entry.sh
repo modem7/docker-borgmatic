ac5b2c257d1f3fbaf614fdec3ad8e639756b24ea
modem7 <modem7@gmail.com>
2022-08-21 02:04:05 +0100

    update

--- base-dockercli/entry.sh
+++ base-dockercli/entry.sh
@@ -12,15 +12,28 @@ echo borgmatic $borgmaticver
 echo $borgver
 echo apprise $apprisever
 
-# Generate Cron variables
-CRON="${CRON:-"0 1 * * *"}"
-CRON_COMMAND="${CRON_COMMAND:-"borgmatic --stats -v 0 2>&1"}"
+# Disable cron if it's set to disabled.
+if [[ "$CRON" =~ ^(false|disabled|off)$ ]]; then
+    echo "Disabling cron, removing configuration"
+    #crontab -r # quite destructive
+    echo -n > /etc/crontabs/root
+    echo "Cron is now disabled"
+# Apply default or custom cron if $CRON is unset or set (not null):
+elif [[ -v CRON ]]; then
+    CRON="${CRON:-"0 1 * * *"}"
+    CRON_COMMAND="${CRON_COMMAND:-"borgmatic --stats -v 0 2>&1"}"
+    echo "$CRON $CRON_COMMAND" > /etc/crontabs/root
+    echo "Applying custom cron"
+# If nothing is set, revert to default behaviour
+else
+    echo "Applying crontab.txt"
+    crontab /etc/borgmatic.d/crontab.txt
+fi
 
-# Apply cron variables
-echo "$CRON $CRON_COMMAND" > /etc/crontabs/root
+# Apply extra cron if it's set
 if [ -v EXTRA_CRON ]
 then
-   echo "$EXTRA_CRON" >> /etc/crontabs/root
+    echo "$EXTRA_CRON" >> /etc/crontabs/root
 fi
 
 # Current crontab var
@@ -30,4 +43,4 @@ crontab=$(crontab -l)
 printf "Cron job set as: \n$crontab\n"
 
 # Start Cron
-/usr/sbin/crond -f -L /dev/stdout
+crond -f -L /dev/stdout
